674255ff2c1e19a80546a98486906c2cf5964da7
Sirui Wang <siruignaw@gmail.com>
2026-07-16 14:43:07 -0600

    Added working pause detection using Silero VAD, including pre-roll.

--- src/mic_capture.py
+++ src/mic_capture.py
@@ -4,6 +4,7 @@ from scipy.io.wavfile import write
 import queue
 import threading
 import torch
+import collections
 
 model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad', 
                               model='silero_vad', 
@@ -18,22 +19,47 @@ buffer = []
 def callback(indata, frames, time, status):
     if status:
         print(status)
-    data = indata.copy()
-    audio_tensor = torch.from_numpy(data.flatten()).float()
-    speech_prob = model(audio_tensor, fs).item()
-    if speech_prob > 0.5:
-        print("speech")
-        audio_queue.put(indata.copy())
-    else:
-        print("silence")
+    audio_queue.put(indata.copy())
 
 def consumer():
+    state = "waiting"
+    pre_roll = collections.deque(maxlen=7)
+    segment_buffer = []
+    silent_chunk_count = 0
+    SILENCE_THRESHOLD_CHUNKS = 22
+    test_num = 1
     while True:
         chunk = audio_queue.get()
         if chunk is None:
             break
-        buffer.append(chunk)
+        data = chunk.flatten()
+        audio_tensor = torch.from_numpy(data).float()
+        speech_prob = model(audio_tensor, fs).item()
+        is_speech = speech_prob > 0.5
 
+        pre_roll.append(chunk)
+
+        if state == "waiting":
+            if not is_speech:
+                pass
+            else:
+                state = "in_speech"
+                pre_roll_lst = []
+                while len(pre_roll) > 0:
+                    pre_roll_lst.append(pre_roll.popleft())
+                segment_buffer[len(segment_buffer):] = pre_roll_lst
+        elif state == "in_speech":
+            if is_speech:
+                silent_chunk_count = 0
+                segment_buffer.append(chunk)
+            else:
+                silent_chunk_count += 1
+                if silent_chunk_count >= SILENCE_THRESHOLD_CHUNKS:
+                    state = "waiting"
+                    segment_data = np.concatenate(tuple(segment_buffer))
+                    write("scratch/segment_" + str(test_num) + ".wav", fs, segment_data)
+                    segment_buffer = []
+                    test_num+=1
 consumer_thread = threading.Thread(target=consumer)
 consumer_thread.start()
 
@@ -49,7 +75,4 @@ except KeyboardInterrupt:
     stream.close()
     audio_queue.put(None)
     consumer_thread.join()
-    tup = tuple(buffer)
-    fin = np.concatenate(tup)
-    write("scratch/live_test.wav", fs, fin)
 print("done")
